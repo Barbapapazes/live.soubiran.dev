20b449d6ba53adb03369dd4b3cb9a5850ba9aaeb
barbapapazes <e.soubiran25@gmail.com>
2025-05-29 12:46:45 +0200

    chore: lint

--- tests/Unit/Events/ConfettiExplodeTest.php
+++ tests/Unit/Events/ConfettiExplodeTest.php
@@ -3,11 +3,9 @@
 declare(strict_types=1);
 
 use App\Events\ConfettiExplode;
-use Illuminate\Broadcasting\Channel;
 
 it('can broadcast confetti explode event', function () {
     $event = new ConfettiExplode();
 
-    expect($event->broadcastOn()->name)
-        ->toBe(new Channel('live')->name);
+    expect($event->broadcastOn()->name)->toBe('live');
 });
--- tests/Unit/Events/ConfettiLockedTest.php
+++ tests/Unit/Events/ConfettiLockedTest.php
@@ -3,11 +3,9 @@
 declare(strict_types=1);
 
 use App\Events\ConfettiLocked;
-use Illuminate\Broadcasting\Channel;
 
 it('can broadcast confetti locked event', function () {
     $event = new ConfettiLocked();
 
-    expect($event->broadcastOn()->name)
-        ->toBe(new Channel('live')->name);
+    expect($event->broadcastOn()->name)->toBe('live');
 });
