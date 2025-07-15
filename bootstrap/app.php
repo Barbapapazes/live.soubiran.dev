e683bbe6b4594a22eab725907c4c619707133418
barbapapazes <e.soubiran25@gmail.com>
2025-07-15 21:09:45 +0200

    fix: rename xcsrf-token

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\RenameCookies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -18,6 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
             HandleInertiaRequests::class,
+        ], prepend: [
+            RenameCookies::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
